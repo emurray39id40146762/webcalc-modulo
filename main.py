2e31319d93d2458cf2ba38396e73cdccbd027d09
EDEL <edelellenmurray@hotmail.com>
2021-03-26 16:58:52 +0000

    full testing and error handling

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
-def calculate(request):
-    x = 0
-    y = 0
-    r = {
-        'error': False,
-        'string': '',
-        'answer': 0
-    }
-    request_json = request.get_json()
-    try:
-        x += request_json['x']
-        y += request_json['y']
-    except (TypeError, ValueError):
-        r['error'] = True
-        print("Both values have to be integers.")
+import json
 
-    r['string'] = x, '%', y, '=', r['answer']
+
+# https://stackoverflow.com/questions/2262333/is-there-a-built-in-or-more-pythonic-way-to-try-to-parse-a-string-to-an-integer
+def intTryParse(value):
+    try:
+        int(value)
+        return True
+    except ValueError as e:
+        return False
+
+
+def calculate(request):
+    request_args = request.args
+    if not request_args:
+        msg = "No arguments recieved"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    if "x" not in request_args or "y" not in request_args:
+        msg = "x and y value not supplied"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    if "x" in request_args and "y" in request_args:
+        try:
+            intTryParse(request_args['x'])
+            intTryParse(request_args['y'])
+            x = int(request_args['x'])
+            y = int(request_args['y'])
+        except Exception as e:
+            msg = (
+                "Invalid parameters for calculation: "
+                "params are not valid integers"
+            )
+            print(f"error: {e}")
+            return f"Bad Request: {msg}", 400
+
+    r = {
+        'calculation': '',
+        'answer': 0,
+    }
+
+    try:
+        r['answer'] = x % y
+        r['calculation'] = x, '%', y
+    except Exception as e:
+        print(f"error: {e}")
+        return "", 500
+
     # Set CORS headers for the main request
     headers = {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json'
     }
-    if r['error']:
-        return r, 400, headers
-    else:
-        return r, 200, headers
+    output = json.dumps(r)
+
+    return output, 200, headers
